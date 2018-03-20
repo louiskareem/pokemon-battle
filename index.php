b1c086dc9852e1f992a7826bda14de599d65e100
louiskareem <99043889@mydavinci.nl>
2018-03-20 09:10:57 +0100

    v1 of the PokeBattle

--- index.php
+++ index.php
@@ -4,10 +4,14 @@ require 'Pokemon.php';
 require 'Pikachu.php';
 require 'Charmeleon.php';
 
-	$pikachu = new Pikachu('Pikachu', 'Lighting', 60);
-	$pikachu->weakness = ['Fire', 1.5];
-	$pikachu->attacks = ['electricRing', 50, 'pikaPunch', 20];
-	$pikachu->resistance = ['Fighting', 20];
+	// $pikachu = new Pikachu('Pikachu', 'Lighting', 60);
+	// $pikachu->weakness = ['Fire', 1.5];
+	// $pikachu->attacks = ['electricRing', 50, 'pikaPunch', 20];
+	// $pikachu->resistance = ['Fighting', 20];
 
-	return $pikachu;
+	// return $pikachu;
+
+	$array = array('hello', 'bye');
+	var_dump($array);
+
 ?>
